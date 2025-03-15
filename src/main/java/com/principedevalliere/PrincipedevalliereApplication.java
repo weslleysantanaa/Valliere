f27ee14fb7945921c1e4fa0e77b4a78b4a102844
wsllyz <westvsantana@gmail.com>
2025-03-15 13:54:15 -0300

    spring initializr

---
+++ src/main/java/com/principedevalliere/PrincipedevalliereApplication.java
@@ -0,0 +1,13 @@
+package com.principedevalliere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincipedevalliereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincipedevalliereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/principedevalliere/PrincipedevalliereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.principedevalliere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincipedevalliereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
